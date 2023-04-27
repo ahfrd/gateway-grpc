bb1d734b478b46745b32708c7166c44c6f72e009
ahmad.farid <a.farid@indocyber.id>
2023-04-27 14:53:07 +0700

    Added paymentRequest topup

--- src/model/request/payment_topup_request.go
+++ src/model/request/payment_topup_request.go
@@ -1,1 +1,11 @@
 package request
+
+type PaymentRequest struct {
+	Method          string           `json:"method"`
+	Nominal         string           `json:"nominalTopUp"`
+	Fee             string           `json:"feeTopUp"`
+	TransactionCode string           `json:"transactionCode"`
+	PhoneNumb       string           `json:"phoneNumber"`
+	Card            DebitCardRequest `json:"cardInfo"`
+	CodeNumb        string           `json:"codeNumber"`
+}
