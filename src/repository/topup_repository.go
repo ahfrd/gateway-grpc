b2a0127e1c62fae046eb8893454d78d1155e3aa5
ahmad.farid <a.farid@indocyber.id>
2023-04-27 14:53:19 +0700

    Added repository for topup

--- src/repository/topup_repository.go
+++ src/repository/topup_repository.go
@@ -1,1 +1,12 @@
 package repository
+
+import (
+	"github.com/ahfrd/gateway-apps-grpc/src/model/request"
+	proto "github.com/ahfrd/gateway-apps-grpc/src/proto/topup"
+)
+
+type TopUpRepository interface {
+	Form(bodyReq request.FormRequest) (*proto.GeneralResponse, error)
+	Inquiry(bodyReq request.InquiryRequest) (*proto.GeneralResponse, error)
+	Payment(bodyReq request.PaymentRequest) (*proto.GeneralResponse, error)
+}
--- src/repository/topup_repository_impl.go
+++ src/repository/topup_repository_impl.go
@@ -1,1 +1,66 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/ahfrd/gateway-apps-grpc/src/model/request"
+	proto "github.com/ahfrd/gateway-apps-grpc/src/proto/topup"
+)
+
+func NewTopUpRepository() TopUpRepository {
+	return &topUpRepositoryImpl{}
+}
+
+type topUpRepositoryImpl struct {
+	ProtoTopUp proto.TopUpServiceClient
+}
+
+func (repository *topUpRepositoryImpl) Form(bodyReq request.FormRequest) (*proto.GeneralResponse, error) {
+	payload := &proto.FormRequest{
+		PhoneNumber: bodyReq.PhoneNumber,
+	}
+	res, err := repository.ProtoTopUp.Form(context.Background(), payload)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+func (repository *topUpRepositoryImpl) Inquiry(bodyReq request.InquiryRequest) (*proto.GeneralResponse, error) {
+	payload := &proto.InquiryRequest{
+		Method:       bodyReq.Method,
+		PhoneNumber:  bodyReq.PhoneNumb,
+		NominalTopUp: bodyReq.Nominal,
+		FeeTopUp:     bodyReq.Fee,
+		CardInfo: &proto.DebitCardRequest{
+			NomorKartu:  bodyReq.Card.CardNumb,
+			ValidUntil:  bodyReq.Card.Valid,
+			SecurityNum: bodyReq.Card.SecurityNum,
+		},
+		CodeNumber: bodyReq.CodeNumb,
+		BankCode:   bodyReq.BankCode,
+	}
+	res, err := repository.ProtoTopUp.Inquiry(context.Background(), payload)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+func (repository *topUpRepositoryImpl) Payment(bodyReq request.PaymentRequest) (*proto.GeneralResponse, error) {
+	payload := &proto.PaymentRequest{
+		Method:       bodyReq.Method,
+		PhoneNumber:  bodyReq.PhoneNumb,
+		NominalTopUp: bodyReq.Nominal,
+		FeeTopUp:     bodyReq.Fee,
+		CardInfo: &proto.DebitCardRequest{
+			NomorKartu:  bodyReq.Card.CardNumb,
+			ValidUntil:  bodyReq.Card.Valid,
+			SecurityNum: bodyReq.Card.SecurityNum,
+		},
+		CodeNumber: bodyReq.CodeNumb,
+	}
+	res, err := repository.ProtoTopUp.Payment(context.Background(), payload)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
